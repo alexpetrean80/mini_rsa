60aa7636a70113627685a1aaa0601d0682eefe4b
Katherine_KP <ioana.iulia.katherine@gmail.com>
2022-01-11 02:21:42 +0200

    generating p and q and testing with miller-rabin

---
+++ key_generation.py
@@ -0,0 +1,74 @@
+# Large Prime Generation for RSA
+import random
+
+# List of primes
+first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                     31, 37, 41, 43, 47, 53, 59, 61, 67,
+                     71, 73, 79, 83, 89, 97, 101, 103,
+                     107, 109, 113, 127, 131, 137, 139,
+                     149, 151, 157, 163, 167, 173, 179,
+                     181, 191, 193, 197, 199, 211, 223,
+                     227, 229, 233, 239, 241, 251, 257,
+                     263, 269, 271, 277, 281, 283, 293,
+                     307, 311, 313, 317, 331, 337, 347, 349]
+
+
+def n_random(n):
+    return random.randrange(2 ** (n - 1) + 1, 2 ** n - 1)
+
+
+def get_low_level_prime(n):
+    # Generating a prime candidate divisible by first primes
+    while True:
+        # Obtain a random number
+        pc = n_random(n)
+
+        # Test divisibility by pre-generated primes
+        for divisor in first_primes_list:
+            if pc % divisor == 0 and divisor ** 2 <= pc:
+                break
+        else:
+            return pc
+
+
+def is_miller_rabin_passed(mrc):
+    # 20 iterations of Rabin Miller Primality test
+    maxDivisionsByTwo = 0
+    ec = mrc - 1
+    while ec % 2 == 0:
+        ec >>= 1
+        maxDivisionsByTwo += 1
+    assert (2 ** maxDivisionsByTwo * ec == mrc - 1)
+
+    def trial_composite(round_tester):
+        if pow(round_tester, ec, mrc) == 1:
+            return False
+        for i in range(maxDivisionsByTwo):
+            if pow(round_tester, 2 ** i * ec, mrc) == mrc - 1:
+                return False
+        return True
+
+    numberOfRabinTrials = 20
+    for i in range(numberOfRabinTrials):
+        round_tester = random.randrange(2, mrc)
+        if trial_composite(round_tester):
+            return False
+    return True
+
+
+def generating_primes(n):
+    ok = 0
+    while True:
+        f = open("p_and_q.txt", "a")
+        prime_candidate = get_low_level_prime(n)
+        if not is_miller_rabin_passed(prime_candidate):
+            continue
+        else:
+            f.write(str(prime_candidate) + '\n')
+            ok += 1
+        if ok == 2:
+            break
+
+
+# if __name__ == '__main__':
+#    generating_primes(300)
